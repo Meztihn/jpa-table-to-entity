2f74ccfd23be5452c24220123d773acc37aaa63f
meztihn <meztihn@gmail.com>
2018-06-15 09:40:54 +0500

    add logging

--- src/main/kotlin/meztihn/jpa/convert/entity/ExceptionHandlingEventQueue.kt
+++ src/main/kotlin/meztihn/jpa/convert/entity/ExceptionHandlingEventQueue.kt
@@ -2,17 +2,21 @@ package meztihn.jpa.convert.entity
 
 import meztihn.jpa.convert.entity.view.showError
 import net.sf.jsqlparser.JSQLParserException
+import org.apache.logging.log4j.LogManager
 import java.awt.AWTEvent
 import java.awt.EventQueue
 import java.security.PrivilegedActionException
 
 object ExceptionHandlingEventQueue : EventQueue() {
+    private val logger = LogManager.getLogger()
+
     override fun dispatchEvent(event: AWTEvent?) {
         try {
             super.dispatchEvent(event)
         } catch (exception: Exception) {
+            logger.catching(exception)
             val cause = realCauseOf(exception)
-            showError("Unexpected error", cause.message ?: "No message provided.")
+            showError("Unexpected error", cause.message ?: "No meaningful message found. See logs/app.log for details.")
         }
     }
 
